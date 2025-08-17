495e97f118c060f3dfe2cbcaecf5334fbcbffc15
MedveMarci <martonmende@gmail.com>
2025-08-17 14:25:55 +0200

    Added config

--- StatsSystem/Config.cs
+++ StatsSystem/Config.cs
@@ -6,4 +6,25 @@ internal class Config
 {
     [Description("Enable or disable the debug mode. When enabled, additional debug information will be logged.")]
     public bool Debug { get; set; } = false;
+    
+    [Description("Enable or disable the playtime tracking feature. When enabled, player playtime will be tracked and saved.")]
+    public bool PlaytimeTracking { get; set; } = true;
+    
+    [Description("Enable or disable the kills tracking feature. When enabled, player kills will be tracked and saved.")]
+    public bool KillsTracking { get; set; } = true;
+    
+    [Description("Enable or disable the deaths tracking feature. When enabled, player deaths will be tracked and saved.")]
+    public bool DeathsTracking { get; set; } = true;
+    
+    [Description("Enable or disable the Class D kills tracking feature. When enabled, kills made by Class D players will be tracked and saved.")]
+    public bool KillsAsClassDTracking { get; set; } = true;
+    
+    [Description("Enable or disable the SCP kills tracking feature. When enabled, kills made by SCPs will be tracked and saved.")]
+    public bool ClassDKillsTracking { get; set; } = true;
+    
+    [Description("Enable or disable the SCP kills tracking feature. When enabled, kills made by SCPs will be tracked and saved.")]
+    public bool ScpKillsTracking { get; set; } = true;
+    
+    [Description("Enable or disable the MicroHID kills tracking feature. When enabled, kills made by MicroHID will be tracked and saved.")]
+    public bool MicroHidKillsTracking { get; set; } = true;
 }
--- StatsSystem/Events/EventHandler.cs
+++ StatsSystem/Events/EventHandler.cs
@@ -17,6 +17,7 @@ internal class EventHandler : CustomEventsHandler
     
     public override void OnPlayerJoined(PlayerJoinedEventArgs ev)
     {
+        if (!StatsSystemPlugin.Instance.Config.PlaytimeTracking) return;
         PlayerJoinTimes[ev.Player.UserId] = DateTime.Now;
         LogManager.Debug($"Player {ev.Player.UserId} joined at {PlayerJoinTimes[ev.Player.UserId]}");
         base.OnPlayerJoined(ev);
@@ -24,6 +25,7 @@ internal class EventHandler : CustomEventsHandler
 
     public override void OnPlayerLeft(PlayerLeftEventArgs ev)
     {
+        if (!StatsSystemPlugin.Instance.Config.PlaytimeTracking) return;
         if (!PlayerJoinTimes.TryRemove(ev.Player.UserId, out var joinTime)) return;
         var playTime = DateTime.Now - joinTime;
         ev.Player.AddDuration("TotalPlayTime", playTime);
@@ -34,30 +36,35 @@ internal class EventHandler : CustomEventsHandler
     public override void OnPlayerDeath(PlayerDeathEventArgs ev)
     {
         LogManager.Debug($"Player {ev.Player.UserId} died. Attacker: {ev.Attacker?.UserId ?? "None"}");
-        ev.Attacker?.IncrementStat("Kills");
-        ev.Player.IncrementStat("Deaths");
-        if (ev.OldRole is RoleTypeId.ClassD) 
+        if (StatsSystemPlugin.Instance.Config.KillsTracking)
+            ev.Attacker?.IncrementStat("Kills");
+        if (StatsSystemPlugin.Instance.Config.DeathsTracking)
+            ev.Player.IncrementStat("Deaths");
+        if (ev.OldRole is RoleTypeId.ClassD && StatsSystemPlugin.Instance.Config.ClassDKillsTracking) 
             ev.Attacker?.IncrementStat("ClassDKills");
-        if (ev.Attacker?.Role is RoleTypeId.ClassD)
+        if (ev.Attacker?.Role is RoleTypeId.ClassD && StatsSystemPlugin.Instance.Config.KillsAsClassDTracking)
             ev.Attacker?.IncrementStat("KillsAsClassD");
-        if (ev.OldRole.IsScp())
+        if (ev.OldRole.IsScp() && StatsSystemPlugin.Instance.Config.ScpKillsTracking)
             ev.Attacker?.IncrementStat("ScpKills");
-        if (ev.DamageHandler is MicroHidDamageHandler)
+        if (ev.DamageHandler is MicroHidDamageHandler && StatsSystemPlugin.Instance.Config.MicroHidKillsTracking)
             ev.Attacker?.IncrementStat("MicroHidKills");
         base.OnPlayerDeath(ev);
     }
     
     internal static void OnQuit()
     {
-        foreach (var kvp in PlayerJoinTimes)
+        if (StatsSystemPlugin.Instance.Config.PlaytimeTracking)
         {
-            var player = Player.Get(kvp.Key);
-            if (player == null) continue;
-            var playTime = DateTime.Now - kvp.Value;
-            player.AddDuration("TotalPlayTime", playTime);
+            foreach (var kvp in PlayerJoinTimes)
+            {
+                var player = Player.Get(kvp.Key);
+                if (player == null) continue;
+                var playTime = DateTime.Now - kvp.Value;
+                player.AddDuration("TotalPlayTime", playTime);
+            }
+
+            PlayerJoinTimes.Clear();
         }
-        
-        PlayerJoinTimes.Clear();
         StatsSystemPlugin.StatsSystem.SaveStats();
         LogManager.Debug("Player stats saved on quit.");
         Shutdown.OnQuit -= OnQuit;
